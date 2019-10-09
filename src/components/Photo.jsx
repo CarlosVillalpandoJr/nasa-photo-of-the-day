487247af0962bad1ab661c58fd294f46de7656da
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-10-09 14:51:47 -0500

    created Photo and PhotoInfo components

---
+++ src/components/Photo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Photo(props) {
+return (
+    <div>
+        <img src='' alt='NASA Photo of the Day' />
+    </div>
+    )
+}
---
+++ src/components/PhotoInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PhotoInfo(props) {
+    return (
+        <div>
+
+        </div>
+    )
+}
